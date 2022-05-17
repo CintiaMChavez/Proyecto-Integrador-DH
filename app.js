51f385cf75b01e742063bf245c176887e582e278
cintia1522 <cintiamchavez@gmail.com>
2022-05-17 12:21:15 -0300

    vista en proceso

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
-const express= require("express");
-const path= require("path");
-const app= express();
+const express = require("express");
+const path = require("path");
+const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // llamando_servidor
-app.listen(8080, () => {
-    console.log("Servidor corriendo!!!💻");
-})
-// Rutas
+app.listen(3030, () => {
+        console.log("Servidor corriendo!!!💻");
+    })
+    // Rutas
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/index.html"));
 })
 app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/login.html"));
-    
+
 })
 app.get("/product_Cart", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/product_Cart.html"));
-    
+
 })
 app.get("/product_Detail", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/product_Detail.html"));
-    
+
 })
 app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/register.html"));
-    
-})
 
+})
