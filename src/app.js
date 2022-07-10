d27e1ee87dbee053202a847e6a0ebda3ff2a019c
bflavia272 <bflavia272@gmail.com>
2022-07-10 11:40:54 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const routes = require("./routes/routes");
 const productsRoutes = require('./routes/productsRoutes');
 const mdProducts = require("./middlewares/mdProducts");
 const app = express();
-
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 const port = process.env.PORT || 3000;
